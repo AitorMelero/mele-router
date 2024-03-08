35e1c7c1f94cec02cb669dbfecb04b6871c9f7db
AitorMelero <meleropiconaitor@gmail.com>
2024-03-08 09:49:13 +0100

    feat: Add lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
+import { lazy, Suspense } from 'react'
+
 import './App.css'
-import HomePage from './pages/Home'
-import AboutPage from './pages/About'
+// import HomePage from './pages/Home'
+// import AboutPage from './pages/About' // static import
 import Page404 from './pages/404'
 import SearchPage from './pages/Search'
 
 import { Router } from './components/Router'
 import { Route } from './components/Route'
 
+const LazyAboutPage = lazy(() => import('./pages/About.jsx')) // lazy import
+const LazyHomePage = lazy(() => import('./pages/Home.jsx')) // lazy import
+
 const appRoutes = [
   {
     path: '/',
-    Component: HomePage
+    Component: LazyHomePage
   },
   {
     path: '/about',
-    Component: AboutPage
+    Component: LazyAboutPage
   },
   {
     path: '/search/:query',
@@ -25,10 +30,12 @@ const appRoutes = [
 function App () {
   return (
     <main>
-      <Router routes={appRoutes} defaultComponent={Page404}>
-        <Route path='/' Component={HomePage} />
-        <Route path='/about' Component={AboutPage} />
-      </Router>
+      <Suspense fallback={null}>
+        <Router routes={appRoutes} defaultComponent={Page404}>
+          <Route path='/' Component={LazyHomePage} />
+          <Route path='/about' Component={LazyAboutPage} />
+        </Router>
+      </Suspense>
     </main>
   )
 }
